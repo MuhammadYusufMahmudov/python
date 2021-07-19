fb493ef91901e1715eeac7f144134b0a42886e75
MuhammadYusuf <mahmudovmuhammad7059@gmail.com>
2021-07-19 12:33:23 +0500

    birinchi mashq
    birinchi mashq yuklanyapdi

---
+++ temp.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+# n=1 # ismlarni sanash uchun o'zgaruvchi
+# while True:
+#     savol = f"{n}-do'stingiz ismini kiriting:"
+#     ism = input(savol)
+#     ismlar.append(ism)
+#     javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
+#     if javob =='ha':
+#         n+=1
+#         continue
+#     else:
+#         break
+
+# print("Do'stlaringiz yoshini saqlaymiz.")
+# dostlar = {}
+# ishora = True
+# while ishora:
+#     ism = input("Do'stingiz ismini kiriting: ")
+#     yosh = input(f"{ism.title()}ning yoshini kiriting: ")
+#     dostlar[ism] = int(yosh) # ism kalit, yosh qiymat
+    
+#     javob = input("Yana ma'lumot qo'shasizmi? (ha/yo'q)")
+#     if javob == "yo'q":
+#         ishora = False
+
+# for ism, yosh in dostlar.items():
+#     print(f"{ism.title()} {yosh} yoshda")  
+    
+# cars = ['lacetti','nexia','toyota','nexia','audi','malibu','nexia']
+# while 'nexia' in cars: # toki nexia cars ro'yxati ichida ekan...
+#     cars.remove('nexia') # nexia ni ro'yxatdan olib tashla
+# print(cars)    
+    
+
+# talabalar = ['hasan', 'husan', 'olim', 'botir']
+# baholangan_talabalar = {}
+# while talabalar:
+#     talaba = talabalar.pop()
+#     baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+#     print(f"{talaba.title()} baholandi")
+#     baholangan_talabalar[talaba] = baho
+
+
+
+# buyurtmalar = ['olma','anjir','uzum','qovun']
+# mahsulotlar = {'olma':20000,
+#                'shaftoli':25000,
+#                'tarvuz':18000,
+#                'uzum':22000}
+
+# while buyurtmalar:
+#     buyurtma = buyurtmalar.pop()
+#     if buyurtma in mahsulotlar.keys():
+#         narh = mahsulotlar[buyurtma]
+#         print(f"{buyurtma.title()} - {narh} so'm")
+#     else:
+#         print(f"Bizda {buyurtma} yo'q")
+
+
+
